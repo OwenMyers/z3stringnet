6948e2cc28998b5e35d883e94cd24c106afd2d0a
OwenMyers <owendalemyers@gmail.com>
2017-12-01 08:11:06 -0500

    have lattice struct and can write vrt and hrz dimers

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,36 @@ use std::io::prelude::*;
 use std::fs::File;
 use std::path::Path;
 
-fn write_2d_vec(vec: &mut Vec<u32>) -> Result {
+fn write_vec(f_str: String, vec: &Vec<u8>) {
+    let path = Path::new(&f_str);
+    let display = path.display();
 
-    for i in &mut vec{
-        for j in i{
-            println!("{}", j);
-        }
+    let mut file = match File::create(&path){
+        Err(err) => panic!("could not create {}: {}",
+                           display,
+                           err.description()),
+        Ok(good_file) => good_file,
+    };
+    
+    let mut out_string = String::new();
+    for i in vec{
+        out_string.push_str(&format!("{} ", i));
+    }
+    out_string.push_str("\n");
+    println!("{}", out_string);
+
+    match file.write_all(out_string.as_bytes()){
+        Err(err) => panic!("could not create {}: {}",
+                           display,
+                           err.description()),
+        Ok(_) => println!("fjile out worked"),
     }
 
-    Ok(_)
 
 }
 
-fn main() {
-    
-    let mut vrt = vec![vec![0; 3]; 3];
+fn write_2d_vec(vec: &Vec<Vec<i32>>) {
 
-    //for i in &mut vrt{
-    //    for j in i{
-    //        println!("{}", j);
-    //    }
-    //}
-
-    match write_2d_vec(&vrt){
-        Err(err) => panic!("Could not write vrt"),
-        Ok(_) => println!("Wrote the vector"),
-    }
 
     let path = Path::new("foo.txt");
     let display = path.display();
@@ -40,10 +44,54 @@ fn main() {
         Ok(good_file) => good_file,
     };
 
-    match file.write_all(b"Hi there"){
+    let mut out_string = String::new();
+    for i in vec{
+        for j in i{
+            out_string.push_str(&format!("{} ", j))
+        }
+        out_string.push_str("\n")
+    }
+    println!("{}", out_string);
+
+    match file.write_all(out_string.as_bytes()){
         Err(err) => panic!("could not create {}: {}",
                            display,
                            err.description()),
-        Ok(_) => println!("file out worked"),
+        Ok(_) => println!("fjile out worked"),
     }
+}
+
+struct Lattice {
+    hrz: Vec<u8>,
+    vrt: Vec<u8>,
+    lx: u64,
+    ly: u64,
+}
+
+fn write_lattice(lat: &Lattice) {
+    write_vec(String::from("vrt.txt"), &lat.vrt);
+    write_vec(String::from("hrz.txt"), &lat.hrz);
+}
+
+fn main() {
+
+    let mut lat: Lattice = Lattice {
+        hrz: Vec::new(),
+        vrt: Vec::new(),
+        lx: 2,
+        ly: 2,
+    };
+
+    lat.hrz.push(0);
+    lat.hrz.push(0);
+    lat.vrt.push(0);
+    lat.vrt.push(0);
+    
+    write_lattice(&lat);
+    
+    //let mut vrt = vec![vec![0; 4]; 4];
+    //let mut hrz = vec![vec![0; 4]; 4];
+
+    //write_2d_vec(&lat.vrt);
+
 } 
