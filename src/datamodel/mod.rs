23bfb01b1876849a8909f10d8b1bdd2bc77604e7
OwenMyers <owendalemyers@gmail.com>
2017-12-15 08:07:38 -0500

    I think I figured out the best way to operate on a path. See and compleate Z3String struct.

--- src/datamodel/mod.rs
+++ src/datamodel/mod.rs
@@ -54,6 +54,49 @@ impl Update {
     }
 }
 
+pub struct Z3String {
+    /// This borrows a mutable refference to a lattice so it is assumed that an instance of this
+    /// struct will be used to perform some operation on the lattice and then go out of scope so
+    /// the mutable reference can be avaliabel again.
+    pub start_loc: Point,
+    pub cur_loc: Point,
+    pub path: Vec<Point>,
+    lat: &mut Lattice, 
+}
+impl Z3String {
+    pub fn step(&self, direction: Direction) {
+        /// This function takes a step along a path from the self.cur_loc position to a new
+        /// position determined by the input from the user. It onle steps across one link and it
+        /// CHANGES that link acording to the raising and lowing rules given the orientation of the
+        /// link and the direction of the step.
+        
+        // If the lnik is a real one (point is of the stored sub lattice) then you just need
+        // to call lat `get_link_from_point` and operate on that link
+        if *lat.point_real(cur_loc){
+            match direction {
+                Direction::N => out_raise(cur_loc),
+                Direction::E => out_raise(cur_loc),
+                Direction::S => out_raise(cur_loc),
+                Direction::W => out_raise(cur_loc),
+            }
+            advance cur_loc
+        }
+        // If the link is not real then step in `direction`, which will guarentee you are now on
+        // the reall sublattice, and look back accross the link from the new vertex. You have to
+        // be careful operating on the link because it will be in the reverse direcction now that
+        // you have already taken the step.
+        else {
+            advance cur_loc
+            match direction {
+                Direction::N => in_raise(cur_loc),
+                Direction::E => in_raise(cur_loc),
+                Direction::S => in_raise(cur_loc),
+                Direction::W => in_raise(cur_loc),
+            }
+        }
+    }
+}
+
 pub struct Lattice {
     // All links can be defined by the vertices of one sublattice.
     // This means the len of vertices will always be N/2, where N is the
@@ -89,7 +132,7 @@ impl Lattice {
         else {
             // The edges are the tough part to handle
             // TODO
-            panic!("Cant handle implied vertices yet.");
+            panic!("Cant handle implied vertices yet. Not sure if we need to. This functionality may never exist.");
         }
     }
 }
