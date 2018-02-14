9beb53d8652f11acadcc31229c27fe73de678430
owenmyers <OwenDaleMyers@gmail.com>
2018-02-14 08:34:51 -0500

    Updated main with the propper setup.

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,16 @@ fn main() {
     //lat = build_blank_lat(size);
     lat = build_z3_striped_lat(size);
 
+    // number_bins: The number of lines in the data file
+    let number_bins: u64 = 1;
+    // number_measure: How many measurements to average over per bin
+    let number_measure: u64 = 1;
+    // number_update: How many updated before a measurement
+    let number_update: u64 = 1;
+    // for local updates it should be
+    //let number_update: u64 = 2 * lat.size.x * lat.size.y;
+
+
     // Initilize the object to update the lattice
     let mut updater = Update{
         working_loc: BoundPoint{
@@ -42,12 +52,16 @@ fn main() {
     }
 
     // Actual run
-    for i in 0..2 {
-        density_estimator.count_in_out(&lat);
-        density_estimator.write_total_count(String::from(format!("density_estimator_{}.csv", 0)))
-        if write_configurations {
-            write_lattice(String::from(format!("lattice_{}.csv", i)), &lat);
+    for i in 0..number_bins {
+        for j in 0..number_measure {
+            for k in 0..number_update {
+                if write_configurations {
+                    write_lattice(String::from(format!("lattice_{}.csv", i)), &lat);
+                }
+                updater.random_walk_update(&mut lat);
+            }
+            density_estimator.count_in_out(&lat);
+            density_estimator.write_total_count(String::from(format!("density_estimator_{}.csv", 0)))
         }
-        updater.random_walk_update(&mut lat);
     }   
 } 
