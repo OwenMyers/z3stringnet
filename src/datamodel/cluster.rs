98152af87fe5a1c5c22d9cb63212c9dfe0444007
owenmyers <owendalemyers@gmail.com>
2018-12-04 07:50:29 -0500

    Uncommented out lines to keep dev.

--- src/datamodel/cluster.rs
+++ src/datamodel/cluster.rs
@@ -1,95 +1,103 @@
 
-//use super::datamodel::Point;
-//use lattice::Lattice;
-//use Direction;
-//use Vertex;
-//use std::collections::HashMap;
-//
-//
-//fn increment_location(location: &mut Point, direction: &Direction) {
-//    let increment: Option<Point>;
-//    match *direction {
-//        Direction::N => {
-//            increment = Some(Point {x: 0, y: 1});
-//        },
-//        Direction::E => {
-//            increment = Some(Point {x: 1, y: 0});
-//        },
-//        Direction::S => {
-//            increment = Some(Point {x: 0, y: -1});
-//        },
-//        Direction::W => {
-//            increment = Some(Point {x: -1, y: 0});
-//        },
-//    }
-//    match increment {
-//        Some(inc) => location = &mut location + inc,
-//        None => panic!("No step taken for some reason. No increment."),
-//    }
-//    location
-//}
-//
-///// Return a vector of directions where each direction
-///// corresponds to the non empty links of a vertex.
-///// Return is done with option. Pattern match to get the actual vector
-///// and if no non-empty links are found then return None.
-//pub fn directions_of_filled_links(vertex: &Vertex) -> Option<Vec<Direction>> {
-//
-//    non_empty_links = Vec::new();
-//    for direction in Direction::iterator(){
-//        non_empty_links.push(direction)
-//    }
-//    if non_empty_links.len() > 0 {
-//        Some(non_empty_links)
-//    }
-//    else {
-//        None
-//    }
-//
-//}
-//
-//pub fn watch_cluster(lat: &Lattice) {
-//    // loop over the points in the lattice
-//    // Keep track of the points we have already visited.
-//    let points_visited = Vec::new();
-//    let mut keep_going= true;
-//    let mut completed_vertices = HashMap::new();
-//    for vertex in lat.vertices{
-//
-//        let mut working_loc: Point = vertex.xy.clone();
-//        let mut filled_directions_vec: Vec<Direction>
-//            = match directions_of_filled_links(&vertex) {
-//                None => continue,
-//                Some(filled_vec) => filled_vec
-//            };
-//        let completed_bool
-//            = match completed_vertices.get(&working_loc) {
-//                None => _,
-//                Some(completed_bool) => completed_bool
-//            };
-//        if completed_bool {continue};
-//
-//        keep_going = true;
-//        let stack: Vec<(Point, Vec<Direction>)> = vec![(
-//            working_loc.clone(),
-//            filled_directions_vec.clone()
-//        )];
-//        while keep_going {
-//
-//            (working_loc, filled_directions_vec) = stack.pop();
-//
-//            step_in_direction = filled_directions_vec.pop();
-//            working_loc = increment_location(working_loc, &step_in_direction);
-//
-//            // Because we are looping over vertices on only one sublattice then we just
-//            // need to make sure the vertex has not been clustered or looked at yet
-//            // Check if the vertex has links
-//            filled_directions_vec = match directions_of_filled_links(&vertex) {
-//                Some(cur_filled) => cur_filled,
-//                None => {
-//                    SOMETHING SPECIAL HAPPENS HERE. NEED TO POP
-//                }
-//            };
+use super::datamodel::Point;
+use lattice::Lattice;
+use Direction;
+use Vertex;
+use std::collections::HashMap;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn increment_location() {
+        assert!(true);
+    }
+}
+
+
+fn increment_location(location: &mut Point, direction: &Direction) {
+    let increment: Option<Point>;
+    match *direction {
+        Direction::N => {
+            increment = Some(Point {x: 0, y: 1});
+        },
+        Direction::E => {
+            increment = Some(Point {x: 1, y: 0});
+        },
+        Direction::S => {
+            increment = Some(Point {x: 0, y: -1});
+        },
+        Direction::W => {
+            increment = Some(Point {x: -1, y: 0});
+        },
+    }
+    match increment {
+        Some(inc) => location = &mut location + inc,
+        None => panic!("No step taken for some reason. No increment."),
+    }
+    location
+}
+
+/// Return a vector of directions where each direction
+/// corresponds to the non empty links of a vertex.
+/// Return is done with option. Pattern match to get the actual vector
+/// and if no non-empty links are found then return None.
+pub fn directions_of_filled_links(vertex: &Vertex) -> Option<Vec<Direction>> {
+
+    non_empty_links = Vec::new();
+    for direction in Direction::iterator(){
+        non_empty_links.push(direction)
+    }
+    if non_empty_links.len() > 0 {
+        Some(non_empty_links)
+    }
+    else {
+        None
+    }
+
+}
+
+pub fn watch_cluster(lat: &Lattice) {
+    // loop over the points in the lattice
+    // Keep track of the points we have already visited.
+    let points_visited = Vec::new();
+    let mut keep_going= true;
+    let mut completed_vertices = HashMap::new();
+    for vertex in lat.vertices{
+
+        let mut working_loc: Point = vertex.xy.clone();
+        let mut filled_directions_vec: Vec<Direction>
+            = match directions_of_filled_links(&vertex) {
+                None => continue,
+                Some(filled_vec) => filled_vec
+            };
+        let completed_bool
+            = match completed_vertices.get(&working_loc) {
+                None => _,
+                Some(completed_bool) => completed_bool
+            };
+        if completed_bool {continue};
+
+        keep_going = true;
+        let stack: Vec<(Point, Vec<Direction>)> = vec![(
+            working_loc.clone(),
+            filled_directions_vec.clone()
+        )];
+        while keep_going {
+
+            (working_loc, filled_directions_vec) = stack.pop();
+
+            step_in_direction = filled_directions_vec.pop();
+            working_loc = increment_location(working_loc, &step_in_direction);
+
+            // Because we are looping over vertices on only one sublattice then we just
+            // need to make sure the vertex has not been clustered or looked at yet
+            // Check if the vertex has links
+            filled_directions_vec = match directions_of_filled_links(&vertex) {
+                Some(cur_filled) => cur_filled,
+                None => {
+                    SOMETHING SPECIAL HAPPENS HERE. NEED TO POP
+                }
+            };
             stack.push(
                 (working_loc.clone(), filled_directions_vec.clone())
             );
