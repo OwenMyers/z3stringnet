020706f625776551b82eb417d7da57fec3f743ef
owenmyers <owendalemyers@gmail.com>
2020-06-29 23:19:35 -0400

    Moved some stuff to new gui mod.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
 extern crate rand;
+#[macro_use]
+extern crate conrod_core;
+extern crate glium;
+extern crate conrod_winit;
+extern crate conrod_glium;
 pub mod datamodel;
 pub mod lattice_updates;
 pub mod estimators;
 pub mod oio;
+pub mod gui;
+
 
 #[cfg(test)]
 mod tests {
